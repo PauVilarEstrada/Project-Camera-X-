8f515e10b8c792aa98161e348ff6e9384164f0b1
Pau Vilar I Estrada <148791785+PauVilarEstrada@users.noreply.github.com>
2024-02-16 16:37:07 +0100

    pruebas

--- app/src/main/java/com/pau/camerax/MainActivity.java
+++ app/src/main/java/com/pau/camerax/MainActivity.java
@@ -1,78 +1,179 @@
 package com.pau.camerax;
 
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Environment;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ProcessCameraProvider;
 
-import com.pau.camerax.databinding.ActivityMainBinding;
+import com.google.common.util.concurrent.ListenableFuture;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_VIDEO_CAPTURE = 2;
+    private static final int REQUEST_PICK_FROM_GALLERY = 3;
+
+    private ImageCapture imageCapture;
+    private Preview preview;
+    private Camera camera;
+    private ImageView thumbnailImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        thumbnailImageView = findViewById(R.id.thumbnailImageView);
 
-        setSupportActionBar(binding.toolbar);
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 101);
+        }
+    }
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    private boolean allPermissionsGranted() {
+        return ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindCameraUseCases(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private class SurfaceHolderSurfaceProvider implements Preview.SurfaceProvider {
+        private final SurfaceHolder surfaceHolder;
+
+        public SurfaceHolderSurfaceProvider(SurfaceHolder surfaceHolder) {
+            this.surfaceHolder = surfaceHolder;
+        }
+
+        @Override
+        public void onSurfaceRequested(@NonNull Preview.SurfaceRequest surfaceRequest) {
+            // Respond to the surface request by providing the SurfaceHolder's surface
+            surfaceRequest.provideSurface(surfaceHolder.getSurface(), ContextCompat.getMainExecutor(MainActivity.this), result -> {});
+        }
+    }
+
+    private void bindCameraUseCases(ProcessCameraProvider cameraProvider) {
+        preview = new Preview.Builder().build();
+        imageCapture = new ImageCapture.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        cameraProvider.unbindAll();
+
+        camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                // Set the surface to the Preview using a custom SurfaceProvider
+                preview.setSurfaceProvider(new SurfaceHolderSurfaceProvider(holder), ContextCompat.getMainExecutor(MainActivity.this));
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+                // Handle surface changes if needed
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                // Handle surface destruction if needed
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void capturePhoto(View view) {
+        File photoFile = createImageFile();
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(photoFile).build();
+
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                // Photo saved successfully, update UI or show a toast
+                updateThumbnail(Uri.fromFile(photoFile));
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                // Photo capture failed, handle the error
+                Log.e("CameraX", "Error capturing photo: " + exception.getMessage());
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private File createImageFile() {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(System.currentTimeMillis());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = null;
+        try {
+            imageFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
+        return imageFile;
     }
 
+    private void updateThumbnail(Uri uri) {
+        // Load the captured photo thumbnail into the ImageView
+        thumbnailImageView.setImageURI(uri);
+    }
+
+    // Add methods for video capture and gallery button
+
+    // ...
+
+    // Handle permission request result
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 101) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Permissions not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
